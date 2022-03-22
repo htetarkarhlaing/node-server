34ec9de17dbc6e520f779f2fc9f9283993cabb2f
htetarkarhlaing <htet.arkar.hlaing.official.mm@gmail.com>
2022-03-22 11:50:32 +0630

    fixed error

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
-const fetch = require("node-fetch");
-const express = require("express");
-const cors = require("cors");
+import fetch from "node-fetch";
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
 
-cosnt app = express();
+const PORT = process.env.PORT || 3000;
+const app = express();
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-	res.send("Hello World");
-})
+  res.send("Hello World");
+});
 
-app.get("/:params", async (req, res) => {
-	const response = await fetch(req.params.params);
-	res.send(response);	
-})
+app.post("/", async (req, res) => {
+  await fetch(req.body.url)
+    .then((response) => response.json())
+    .then((resJosn) => {
+      res.status(200).send(resJosn);
+    });
+});
 
-app.listen(process.env.PORT||3000, ()=> {
-console.log("server is starting")});
+app.listen(PORT, () => {
+  console.log("server is starting on port", PORT);
+});
