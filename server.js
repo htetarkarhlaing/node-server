bb41403788af2b134b5d833849b0755e13b5d1e0
htetarkarhlaing <htet.arkar.hlaing.official.mm@gmail.com>
2022-05-26 10:54:24 +0630

    added: body-parser

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const fetch = require("cross-fetch");
+const bodyParser = require("body-parser");
 require("dotenv").config();
 
 //PORT
@@ -9,8 +10,8 @@ const PORT = process.env.PORT || 8000;
 const app = express();
 //middlewares
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //routes
 app.get("/", (req, res) => {
